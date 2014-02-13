29447c83f758407098e9b8a1b8ef04dfb54ba2c2
zeroisstart <shenhongmings@gmail.com>
2014-02-13 19:25:32 +0800

    1.修改图片海报

--- protected/modules/Home/views/gallery/post.php
+++ protected/modules/Home/views/gallery/post.php
@@ -1,7 +1,9 @@
 <div id="contextAdorn"></div>
 <div id="contextBox">
 	<div id="rightContent" class="user_profile_info">
-		<img style="width: 650px;" src="<?php echo $this -> createUrl('/')?>/images/海报-02.jpg" />
+		<!-- 
+			<img style="width: 650px;" src="<?php echo $this -> createUrl('/')?>/images/海报-02.jpg" />
+		 -->
 	</div>
 	<div id="leftColumns">
 		<?php $this -> widget('widget.Helper.GallerySecNav');?>
