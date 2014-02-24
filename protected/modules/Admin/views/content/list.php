449a249768e29c37dfd3a5b25988a74af119a3f5
zeroisstart <shenhongmings@gmail.com>
2014-02-24 16:52:11 +0800

    1

--- protected/modules/Admin/views/content/list.php
+++ protected/modules/Admin/views/content/list.php
@@ -48,7 +48,7 @@ $this->widget ( 'widget.Helper.SecNav', array (
 <?php
 /* @var $this AdminController */
 
-$this->widget ( 'widget.helper.GridView', array (
+$this->widget ( 'widget.Helper.GridView', array (
 		'dataProvider' => $dataProvider,
 		'columns' => array (
 				'ID',
